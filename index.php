0142571a9403845e93985b52f6665a177612df86
Benoit MOTTIN <benaesan@msn.com>
2024-11-22 10:23:32 +0100

    [ADD] ajouter le fichier index + style

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+</head>
+<body>
+    <h1>Journal d'une tempete de neige</h1>
+
+    <h2>Jour 1</h2>
+
+    <p>il fait froid et le courant coupe</p>
+
+    <hr>
+    <h2>Jour 2</h2>
+    <p>Il fait toujours froid, et notre formateur nous torture avec GIT, <strong>
+        le sadique</strong></p>
+</body>
+</html>
