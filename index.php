db46472fdedcf85de76e5c38e0587503304a9154
Benoit MOTTIN <benaesan@msn.com>
2024-11-22 10:28:10 +0100

    [UPDATE] ajout jour 3

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./public/css/style.css">
 </head>
+
 <body>
     <h1>Journal d'une tempete de neige</h1>
 
@@ -16,6 +18,10 @@
     <hr>
     <h2>Jour 2</h2>
     <p>Il fait toujours froid, et notre formateur nous torture avec GIT, <strong>
-        le sadique</strong></p>
+            le sadique</strong></p>
+    <hr>
+    <h2>Jour 3</h2>
+    <p>Espérons que le beau temps revienne</p>
 </body>
+
 </html>
